78a976eaf497649444ed437435e0d70e1edcdb7b
dakshmiyani <dakshmiyani111@gmail.com>
2026-01-14 12:58:23 +0530

    delivery status route added

--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -9,6 +9,8 @@ const OrderController =require("../controllers/order.controller");
 
 router.post('/create',Authenticated.isAuthenticated,OrderController.createOrder);
 router.get('/getorder', Authenticated.isAuthenticated,OrderController.getOrder)
+router.put("/delivery-status/:orderId", Authenticated.isAuthenticated,Authenticated.isAdmin, OrderController.updateDeliveryStatus);
+
 
 
 module.exports= router;
