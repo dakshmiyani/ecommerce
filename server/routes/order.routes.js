3a19a0130d63a044a45eff4bf9e2f350df980bcb
dakshmiyani <dakshmiyani111@gmail.com>
2026-01-15 13:49:38 +0530

    cancel order route added

--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -10,6 +10,7 @@ const OrderController =require("../controllers/order.controller");
 router.post('/create',Authenticated.isAuthenticated,OrderController.createOrder);
 router.get('/getorder', Authenticated.isAuthenticated,OrderController.getOrder)
 router.put("/delivery-status/:orderId", Authenticated.isAuthenticated,Authenticated.isAdmin, OrderController.updateDeliveryStatus);
+router.put("/cancel/:orderId", Authenticated.isAuthenticated, OrderController.cancelOrder);
 
 
 
