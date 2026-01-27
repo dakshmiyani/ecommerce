023043d3040b1226a5b636b5da11ebd51f3c74ed
dakshmiyani <dakshmiyani111@gmail.com>
2026-01-27 22:15:22 +0530

    route added

--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -11,7 +11,8 @@ router.post('/create',Authenticated.isAuthenticated,OrderController.createOrder)
 router.get('/getorder', Authenticated.isAuthenticated,OrderController.getOrder)
 router.put("/delivery-status/:orderId", Authenticated.isAuthenticated,Authenticated.isAdmin, OrderController.updateDeliveryStatus);
 router.put("/cancel/:orderId", Authenticated.isAuthenticated, OrderController.cancelOrder);
-router.get("/all",Authenticated.isAuthenticated,Authenticated.isAdmin, OrderController.getAllOrders)
+router.get("/all",Authenticated.isAuthenticated,Authenticated.isAdmin, OrderController.getAllOrders);
+router.get("/getorder/:id", Authenticated.isAuthenticated, OrderController.getOrderbyId);
 
 
 module.exports= router;
